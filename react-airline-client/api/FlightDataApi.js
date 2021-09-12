b4c59421e35a0841050ca0b8efa50c28feba620c
yeji <shfks1212@naver.com>
2021-09-12 23:04:56 +0900

    client

--- react-airline-client/api/FlightDataApi.js
+++ react-airline-client/api/FlightDataApi.js
@@ -1,37 +1,6 @@
-// import flightList from '../resource/flightList';
-// import fetch from 'node-fetch';
-// import { useEffect } from 'react';
-
-// if (typeof window !== 'undefined') {
-//   localStorage.setItem('flight', JSON.stringify(flightList));
-// }
-
-// export function getFlight(filterBy = {}) {
-//   // HINT: 가장 마지막 테스트를 통과하기 위해, fetch를 이용합니다. 아래 구현은 완전히 삭제되어도 상관없습니다.
-//   // TODO: 아래 구현을 REST API 호출로 대체하세요.
-//   let depart = filterBy.departure;
-//   let dest = filterBy.destination;
-//   let param = '';
-
-//   if (dest === undefined) {
-//     param = `departure=${depart}`;
-//   } else {
-//     param = `departure=${depart}&destination=${dest}`;
-//   }
-
-//   return fetch(
-//     `http://ec2-13-124-90-231.ap-northeast-2.compute.amazonaws.com:81/flight?${param}`
-//   )
-//     .then((res) => {
-//       return res.json();
-//     })
-//     .then((datas) => {
-//       return datas;
-//     });
-// }
-
 import flightList from '../resource/flightList';
 import fetch from 'node-fetch';
+import { useEffect } from 'react';
 
 if (typeof window !== 'undefined') {
   localStorage.setItem('flight', JSON.stringify(flightList));
@@ -40,27 +9,58 @@ if (typeof window !== 'undefined') {
 export function getFlight(filterBy = {}) {
   // HINT: 가장 마지막 테스트를 통과하기 위해, fetch를 이용합니다. 아래 구현은 완전히 삭제되어도 상관없습니다.
   // TODO: 아래 구현을 REST API 호출로 대체하세요.
+  let depart = filterBy.departure;
+  let dest = filterBy.destination;
+  let param = '';
 
-  let json = [];
-  if (typeof window !== 'undefined') {
-    json = localStorage.getItem('flight');
+  if (dest === undefined) {
+    param = `departure=${depart}`;
+  } else {
+    param = `departure=${depart}&destination=${dest}`;
   }
-  const flight = JSON.parse(json) || [];
 
-  return new Promise((resolve) => {
-    const filtered = flight.filter((flight) => {
-      let condition = true;
-      if (filterBy.departure) {
-        condition = condition && flight.departure === filterBy.departure;
-      }
-      if (filterBy.destination) {
-        condition = condition && flight.destination === filterBy.destination;
-      }
-      return condition;
+  return fetch(
+    `http://ec2-13-124-90-231.ap-northeast-2.compute.amazonaws.com:81/flight?${param}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((datas) => {
+      return datas;
     });
-
-    setTimeout(() => {
-      resolve(filtered);
-    }, 500);
-  });
 }
+
+// import flightList from '../resource/flightList';
+// import fetch from 'node-fetch';
+
+// if (typeof window !== 'undefined') {
+//   localStorage.setItem('flight', JSON.stringify(flightList));
+// }
+
+// export function getFlight(filterBy = {}) {
+//   // HINT: 가장 마지막 테스트를 통과하기 위해, fetch를 이용합니다. 아래 구현은 완전히 삭제되어도 상관없습니다.
+//   // TODO: 아래 구현을 REST API 호출로 대체하세요.
+
+//   let json = [];
+//   if (typeof window !== 'undefined') {
+//     json = localStorage.getItem('flight');
+//   }
+//   const flight = JSON.parse(json) || [];
+
+//   return new Promise((resolve) => {
+//     const filtered = flight.filter((flight) => {
+//       let condition = true;
+//       if (filterBy.departure) {
+//         condition = condition && flight.departure === filterBy.departure;
+//       }
+//       if (filterBy.destination) {
+//         condition = condition && flight.destination === filterBy.destination;
+//       }
+//       return condition;
+//     });
+
+//     setTimeout(() => {
+//       resolve(filtered);
+//     }, 500);
+//   });
+// }
