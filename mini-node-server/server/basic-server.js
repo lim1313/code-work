0ccfc53abde207d78667f14756421b600d9c9ab1
yeji <shfks1212@naver.com>
2021-09-12 23:05:12 +0900

    express

--- mini-node-server/server/basic-server.js
+++ mini-node-server/server/basic-server.js
@@ -49,3 +49,41 @@ const defaultCorsHeader = {
   'Access-Control-Allow-Headers': 'Content-Type, Accept',
   'Access-Control-Max-Age': 10,
 };
+
+const express = require('express');
+const PORT = 5000;
+const ip = 'localhost';
+
+//! express
+// const app = express();
+
+// const cors = require('cors');
+
+// const defaultCorsHeader = {
+//   origin: 'http://localhost:3000',
+//   methods: 'GET, POST, PUT, DELETE, OPTIONS',
+//   allowedHeaders: 'Content-Type, Accept',
+//   maxAge: 3,
+// };
+
+// app.use(cors(defaultCorsHeader));
+// app.use(express.json({ strict: false }));
+
+// app.options('/*', cors(defaultCorsHeader));
+
+// app.get('/upper', (req, res, next) => {
+//   res.status(200).json('upper get ok');
+// });
+// app.post('/upper', (req, res, next) => {
+//   res.status(200).json(req.body.toUpperCase());
+// });
+// app.post('/lower', (req, res, next) => {
+//   res.status(200).json(req.body.toLowerCase());
+// });
+// app.use((error, req, res, next) => {
+//   res.status(404).send('system error');
+// });
+
+// app.listen(PORT, ip, () => {
+//   console.log(`http server listen on ${ip}:${PORT}`);
+// });
